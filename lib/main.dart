d3de5a7a8dd47b3c40be70f1b0535ac6485c235d
RAFFF <raflifirdausfalaka@gmail.com>
2022-03-27 22:28:45 +0700

    flutter part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rafff_project/main_screen.dart';
+import 'package:rafff_project/provider/done_tourism_provider.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RAFFF PROJECT',
-      theme: ThemeData(),
-      home: const MainScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => DoneTourismProvider(),
+      child: MaterialApp(
+        title: 'RAFFF PROJECT',
+        theme: ThemeData(),
+        home: MainScreen(),
+      ),
     );
   }
 }
